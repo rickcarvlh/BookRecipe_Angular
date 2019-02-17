a99fadc26b445a192fc4f76d8738504f69b1c627
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-02-17 13:52:20 +0000

    Adding ingredients to Recipes

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 // place where we manage our recipes
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
 
@@ -9,8 +10,15 @@ export class RecipeService {
   // so this can't accessed directly from outside
   private recipes: Recipe[] = [
     new Recipe(
-      'A test Recipe', 'This is a description', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'), new Recipe(
-      'Another test Recipe', 'This is a new description', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+      'Tasty Schnitzel', 'A super-tasty Schnitzel  - just awesome', 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',[
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Big Fat Burger', 'What else you need to say?', 'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg', [
+        new Ingredient(' Buns', 2),
+        new Ingredient('Meat', 1)
+      ]),
   ];
 
 //   to return this array from the outside
