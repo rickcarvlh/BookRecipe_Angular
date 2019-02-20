d6d010a502183bcda030963b8648cce266bd3593
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-02-20 11:43:41 +0000

    Adding navigation with routes to the App

--- recipe-book/src/app/header/header.component.ts
+++ recipe-book/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter,Output} from "@angular/core";
+import {Component } from "@angular/core";
 
 
 @Component({
@@ -7,10 +7,8 @@ import {Component, EventEmitter,Output} from "@angular/core";
 })
   export class HeaderComponent {
 
-  @Output() featuredSelected = new EventEmitter<string>();
 
-  onSelect(feature:string) {
-    this.featuredSelected.emit(feature);
-  }
+
+
 
 }
